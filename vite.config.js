6dd47ef17d4ef89da0b93e2e7040d89a12fa8eae
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2024-03-15 14:13:18 +0000

    chore: introduce `GITPOD_VITE_URL` environment variable

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const extendedViteDevServer = {}
+
+if (process.env.GITPOD_VITE_URL) {
+    extendedViteDevServer.hmr = {
+        protocol: 'wss',
+        host: new URL(process.env.GITPOD_VITE_URL).hostname,
+        clientPort: 443
+    }
+}
 
 export default defineConfig({
+    server: {
+        ...extendedViteDevServer
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
